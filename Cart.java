ccc3d89f3e82c828b7adff0abbed8e3e585b968b
Vinh <congvinh0502@gmail.com>
2026-04-18 20:45:44 +0700

    Refactor: prepare project for lab03

---
+++ Cart.java
@@ -0,0 +1,48 @@
+public class Cart {
+    public static final int MAX_NUMBER_ORDERED = 20;
+
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+    private int qtyOrdered = 0;
+
+    // Add DVD
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBER_ORDERED) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added");
+        } else {
+            System.out.println("The cart is almost full");
+        }
+    }
+
+    // Remove DVD
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        boolean found = false;
+
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i] == disc) {
+                found = true;
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                itemsOrdered[qtyOrdered - 1] = null;
+                qtyOrdered--;
+                System.out.println("The disc has been removed");
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("The disc is not found");
+        }
+    }
+
+    // Total cost
+    public float totalCost() {
+        float total = 0;
+        for (int i = 0; i < qtyOrdered; i++) {
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+}
---
+++ DigitalVideoDisc.java
@@ -0,0 +1,57 @@
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    // Constructor 1
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    // Constructor 2
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    // Constructor 3
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    // Constructor 4
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+}
